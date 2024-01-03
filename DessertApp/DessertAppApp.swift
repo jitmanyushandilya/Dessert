9750372d47241be1f9bfcca35186089ab1186d2a
jshandil <jshandil@usc.edu>
2024-01-02 16:19:57 -0800

    Initial Commit

---
+++ DessertApp/DessertAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertAppApp.swift
+//  DessertApp
+//
+//  Created by Jitmanyu Shandilya on 1/2/24.
+//
+
+import SwiftUI
+
+@main
+struct DessertAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
